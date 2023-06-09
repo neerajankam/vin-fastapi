1d51ff1464efa087c5b38c85b8acb137489678a4
Neeraj Ankam <neerajankam@users-MBP.lan1>
2023-06-09 13:16:17 -0500

    Updated cache code

--- cache/sqlite_cache.py
+++ cache/sqlite_cache.py
@@ -12,10 +12,12 @@ class Cache:
 	def get(self, vin):
 		try:
 			vin_object = self.db_connection.query(VinDBModel).filter_by(vin=vin).one_or_none()
-			vin_object = json.loads(vin_object.vehicle_details)
+			vin_object = json.loads(vin_object.vehicle_details) if vin_object else {}
 		except Exception:
-			logging.exception("Encountered exception while trying to fetch cache value.", extra={"vin": vin})
+			logging.exception("Encountered exception while trying to fetch cache vin.", extra={"vin": vin})
 			vin_object = {}
+		finally:
+			self.db_connection.close()
 		return vin_object
 
 	def set(self, vin, vehicle_details):
@@ -26,7 +28,22 @@ class Cache:
 			self.db_connection.add(vehicle_details)
 			self.db_connection.commit()
 		except Exception:
-			logging.exception("Encountered exception while trying to cache value.", extra={"vin": vin})
+			logging.exception("Encountered exception while trying to cache vin.", extra={"vin": vin})
 			self.db_connection.rollback()
 		finally:
 			self.db_connection.close()
+
+	def delete(self, vin):
+		success = False
+		try:
+			deleted_rows = self.db_connection.query(VinDBModel).filter_by(vin=vin).delete()
+			print("Deleted rows: {}".format(deleted_rows))
+			self.db_connection.commit()
+		except Exception:
+			success = False
+			logging.exception("Encountered exception while trying to delete vin.", extra={"vin":vin})
+			self.db_connection.rollback()
+		finally:
+			self.db_connection.close()
+			success = True if deleted_rows == 1 else False
+			return success
